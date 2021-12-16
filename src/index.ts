c433cdd100680f7b59c2f5244363a0dd544d1e42
Mikhail Ershov <tehnarenok@gmail.com>
2021-12-16 19:49:18 +0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log('Hello ёпта')
+
+window.onload = () => {
+	let head  = document.getElementsByTagName('head')[0];
+    let link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/new_year.css'
+    link.media = 'all';
+    head.appendChild(link);
+
+	let linkIcon = <HTMLLinkElement>document.querySelector("link[rel~='icon']");
+    if(linkIcon) linkIcon.href = 'https://i.pinimg.com/originals/fd/71/7d/fd717d65b03bdf7416f23105917f4c02.png'
+}
