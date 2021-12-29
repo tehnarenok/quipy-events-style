200915e6f40d9ccafd340f29e4a20be3907c0a8a
Mikhail Ershov <tehnarenok@gmail.com>
2021-12-29 20:44:42 +0300

    3

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ window.onload = () => {
     let link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = '/new_year.css';
+    link.href = 'https://quipy-nine.vercel.app/new_year.css';
     link.media = 'all';
     head.appendChild(link);
     let linkIcon = document.querySelector("link[rel~='icon']");
